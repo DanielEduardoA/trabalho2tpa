776b28459b2d2deeb7e1fb5993d1e1047a35c90b
Daniel <danieleduardoalm@gmail.com>
2021-06-23 18:54:21 -0300

    correcao da build

--- src/main/java/br/edu/univas/api/marvel/configuration/RestTemplateErrorHandler.java
+++ src/main/java/br/edu/univas/api/marvel/configuration/RestTemplateErrorHandler.java
@@ -1,7 +1,8 @@
 package br.edu.univas.api.marvel.configuration;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
+import java.util.Scanner;
 
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
@@ -18,10 +19,15 @@ public class RestTemplateErrorHandler implements ResponseErrorHandler {
     @Override
     public void handleError(ClientHttpResponse response) throws IOException {
         if (response.getStatusCode().is5xxServerError() || response.getStatusCode().is4xxClientError()) {
-            byte[] responseBody = response.getBody().readAllBytes();
-            String body = new String(responseBody, StandardCharsets.UTF_8);
+        	String body = toString(response.getBody());
             throw new MarvelApiException(body, response.getStatusCode());
         } 
+    }
+    
+    private String toString(InputStream inputStream) {
+        try (Scanner s = new Scanner(inputStream).useDelimiter("\\A")) {
+			return s.hasNext() ? s.next() : "";
+		}
     }
 
 }
